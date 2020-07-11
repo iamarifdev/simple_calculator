ecc66141624ccf680bf733a941463654a4b89f37
Md. Ariful Islam <arifjmamun24@gmail.com>
2020-07-11 23:29:33 +0600

    Practice added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'simple-calculator';
+  public title = 'simple-calculator';
+  // public number1Control = new FormControl();
+  // public number2Control = new FormControl();
+  public number1: string;
+  public number2: string;
+  public selectedOption: string;
+
+  public total: number;
+
+  public onNumberChange(): void {
+    const a = '';
+    // if (!this.number1) {
+    //    // check empty stirng
+    //    // check undefined
+    //    // check null
+    //    // check true
+    //    // check 0
+    //    // falsy value = '', 0, null, undefined, false
+    //   //  alert('Empty');
+    // }
+    // if (!this.number2) {
+    //   // alert('Empty 2');
+    // }
+    this.calcualte();
+  }
+
+  public optionChange(event: MatSelectChange): void {
+    this.selectedOption = event.value;
+    this.calcualte();
+  }
+
+  private calcualte(): void {
+    if (this.selectedOption) {
+      if (this.selectedOption === 'add') {
+        this.total = (Number(this.number1) || 0) + (Number(this.number2) || 0);
+      }
+      if (this.selectedOption === 'sub') {
+        this.total = (Number(this.number1) || 0) - (Number(this.number2) || 0);
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatFormFieldModule,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'fill' },
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
